f2e573b8a2eaf4c5fda1fbcdae84696b208bae32
AAK <aalorwu@gmail.com>
2020-03-09 10:53:44 +0200

    Dependency updates

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -4,9 +4,16 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<onesignal_flutter/OneSignalPlugin.h>)
+#import <onesignal_flutter/OneSignalPlugin.h>
+#else
+@import onesignal_flutter;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [OneSignalPlugin registerWithRegistrar:[registry registrarForPlugin:@"OneSignalPlugin"]];
 }
 
 @end
